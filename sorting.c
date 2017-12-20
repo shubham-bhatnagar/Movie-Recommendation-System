9887e65226df2e25eb245bd0e317b8218388abb4
Udhav Sharma <udhavs1998@gmail.com>
2017-12-20 23:10:39 +0530

    added sorting

---
+++ sorting.c
@@ -0,0 +1,15 @@
+void sort(int *recommended_movies, double *predicted_ratings, int no_of_recommended_movies){
+	int i=0,j=0;
+	
+	//sorting recommended movies and their ratings from highest rating to lowest
+	for(i=0;i<no_of_recommended_movies;i++){
+		for(j=i+1;j<no_of_recommended_movies;j++){
+			if(predicted_ratings[i]<predicted_ratings[j]){
+				double temp1; int temp2;
+				temp1 = predicted_ratings[i]; temp2 = recommended_movies[i];
+				predicted_ratings[i] = predicted_ratings[j]; recommended_movies[i] = recommended_movies[j];
+				predicted_ratings[j] = temp1; recommended_movies[j] = temp2;
+			}
+		}
+	}
+}
